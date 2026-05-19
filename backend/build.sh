b3f2546706ca27d51f83822600c7e4595f6bdfd4
Gowtham <Gowthamtech11@gmail.com>
2026-05-19 18:27:37 +0530

    Restore build.sh by removing temporary database flush command

--- backend/build.sh
+++ backend/build.sh
@@ -6,7 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py flush --no-input
 
 # Create superuser automatically if environment variables are set
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
